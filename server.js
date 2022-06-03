07be5e1b8f361a7a81c971ffbd5cd5ce43f8dad1
cavicphi <cavicphi@students.zhaw.ch>
2022-06-03 22:02:48 +0200

    2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('DevOpsHerokuDocker - Hello Philip :-)))');
+  res.send('DevOpsHerokuDocker - Hello Philip. Was passiert bei Änderungen? :-)))');
 });
 
 app.listen(PORT, HOST);
